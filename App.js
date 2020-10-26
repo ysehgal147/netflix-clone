ac39f84416958d50c10f0c64d9b799d853f80cbb
Yogesh Sehgal <yogeshsehgal99@rediff.com>
2020-10-26 20:13:59 +0530

    Add all my files

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Row from "./Row";
+import requests from "./requests";
+import "./App.css";
+import Banner from "./Banner";
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
